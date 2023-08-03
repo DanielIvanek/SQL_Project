9380c88481fcce11d79c18c3294a78200eab72a0
Papapapavel <104023826+Papapapavel@users.noreply.github.com>
2023-08-03 17:29:09 +0200

    Add files via upload

---
+++ betaapp0.sql
@@ -0,0 +1,73 @@
+
+#binarni promenna pro vikend/prac. den rocni obdobi 
+SELECT
+    *,
+    CASE 
+        WHEN DAYOFWEEK(date) = 1 OR DAYOFWEEK(date) = 7 THEN 1
+        ELSE 0
+    END AS vikend_flag,
+    CASE 
+        WHEN MONTH(date) IN (3, 4, 5) THEN 0 -- Jaro
+        WHEN MONTH(date) IN (6, 7, 8) THEN 1 -- Léto
+        WHEN MONTH(date) IN (9, 10, 11) THEN 2 -- Podzim
+        WHEN MONTH(date) IN (12, 1, 2) THEN 3 -- Zima
+    END AS rocni_obdobi
+FROM
+    covid19_basic_differences cbd ;
+
+
+SELECT
+	DISTINCT cbd.date,
+	ct.country,
+	cbd.country,
+	cbd.confirmed,
+	ct.tests_performed,
+	c.population,
+	c.population_density 
+FROM
+	(
+	SELECT
+		CASE
+			WHEN country = 'Czechia'
+		THEN 'Czech Republic'
+			WHEN country = 'Russia'
+		THEN 'Russian Federation'
+			WHEN country = 'Democratic Republic of Congo'
+		THEN 'Congo (Kinshasa)'
+			WHEN country = 'South Korea'
+		THEN 'Korea, South'
+			WHEN country = 'Taiwan'
+		THEN 'Taiwan*'
+			WHEN country = 'United States'
+		THEN 'US'
+			WHEN country = 'Libyan Arab Jamahiriya'
+ 		THEN 'Libya'
+			ELSE country
+		END AS country,
+		date,
+		confirmed
+	FROM
+		covid19_basic_differences cbd  
+) AS cbd
+LEFT JOIN covid19_tests ct 
+ON
+	cbd.country = ct.country
+	AND cbd.date = ct.date
+LEFT JOIN countries c 
+ON cbd.country  = c.country ;
+
+
+
+select distinct country
+from  covid19_tests ct 
+
+
+except
+
+select distinct country
+from  covid19_basic_differences cbd
+
+
+
+
+
